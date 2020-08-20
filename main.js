755f8183d73ebbd1ac37ba05182d0fecd70f6125
Daniel <danisolo91@gmail.com>
2020-08-20 23:31:48 +0200

    Change player on each move and check if has won (horiz. and vert. only)

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
 const GameBoard = (() => {
-
     const board = [
         [null, null, null],
         [null, null, null],
@@ -8,9 +6,7 @@ const GameBoard = (() => {
     ];
 
     const getBoard = () => board;
-
     const isOccuped = (i ,j) => board[i][j];
-
     const markBox = (sym, i, j) => board[i][j] = sym;
 
     return { getBoard, isOccuped, markBox };
@@ -28,46 +24,83 @@ const DisplayController = (() => {
                 grid.appendChild(box);
             }
         }
+        loadBoxListeners();
+    };
+
+    const loadBoxListeners = () => {
+        grid.querySelectorAll('.box').forEach(box => {
+            let boxId = box.id.slice(4).split(''); // box position array: [i, j]
+            box.addEventListener('click', () => {
+                if(!GameBoard.isOccuped(...boxId)) {
+                    let sym = Game.getSym();
+                    GameBoard.markBox(sym, ...boxId);
+                    updateBox(sym, box);
+                    if(Game.isWinner(boxId, sym)) {
+                        console.log('Has won!!!');
+                    } else {
+                        Game.changeSym();
+                    }
+                }
+            });
+        });
     };
 
     const updateBox = (sym, box) => box.textContent = sym;
 
-    const getGrid = () => grid.querySelectorAll('.box');
-
-    const getBoxId = (box) => box.id.slice(4).split(''); // box position array: [i, j]
-
-    return { createBoard, getGrid, getBoxId, updateBox };
+    return { createBoard };
 })();
 
-const Player = (name, symbol) => {
-    return { name, symbol };
+const Player = (name, sym) => {
+    return { name, sym };
 }
 
 const Game = (() => {
+
+    let sym = 'X'; // Player with X starts first
+
     const player1 = Player('Player 1', 'X');
     const player2 = Player('Player 2', '0');;
 
     const changePlayer1Name = (name) => player1.name = name;
     const changePlayer2Name = (name) => player2.name = name;
 
-    const loadListeners = () => {
-        DisplayController.getGrid().forEach(box => {
-            let boxId = DisplayController.getBoxId(box);
-            box.addEventListener('click', () => {
-                if(!GameBoard.isOccuped(...boxId)) {
-                    GameBoard.markBox('X', ...boxId);
-                    DisplayController.updateBox('X', box);
-                }
-            });
-        });
-    };
+    const getSym = () => sym;
+
+    const changeSym = () => sym === 'X' ? sym = '0' : sym = 'X';
 
     const start = () => {
         DisplayController.createBoard(GameBoard.getBoard());
-        loadListeners();
     };
 
-    return { start };
+    const isWinner = (boxId, sym) => {
+        let result = false;
+        result = isHorizontalWin(boxId[0], sym);
+        if(result) return result;
+        result = isVerticalWin(boxId[1], sym);
+        if(result) return result;
+        result = isDiagonalWin(boxId, sym);
+        return result;
+    }
+
+    const isHorizontalWin = (row, sym) => {
+        const board = GameBoard.getBoard();
+        let count = 0;
+        for(let i = 0; i < board[row].length; i++) {
+            board[row][i] === sym ? count++ : count = 0;
+        }
+        return count === 3;
+    }
+
+    const isVerticalWin = (col, sym) => {
+        const board = GameBoard.getBoard();
+        let count = 0;
+        for(let i = 0; i < board.length; i++) {
+            board[i][col] === sym ? count++ : count = 0;
+        }
+        return count === 3;
+    }
+
+    return { start, getSym, changeSym, isWinner };
 })();
 
 Game.start();
